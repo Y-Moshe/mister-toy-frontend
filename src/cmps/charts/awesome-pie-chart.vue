23a6c032518663d04b4a184d63d584851ced592d
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-22 21:59:24 +0200

    basic charts

---
+++ src/cmps/charts/awesome-pie-chart.vue
@@ -0,0 +1,31 @@
+<template>
+  <PieChart :chartData="data" :options="options" />
+</template>
+
+<script>
+import { PieChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+export default {
+  name: 'awesome-pie-chart',
+  props: { data: Object },
+  components: { PieChart },
+  data() {
+    return {
+      options: {
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label({ label, parsed }) {
+                return `${label}: ${parsed}%`
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
