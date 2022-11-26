657f149da02b0d572fdd20ca0c358b528679959e
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-26 21:50:27 +0200

    logout feature

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -12,6 +12,9 @@
         <li v-if="user.isAdmin">
           <router-link to="/admin/dashboard">Dashboard</router-link>
         </li>
+        <li>
+          <el-button type="danger" :icon="SwitchButton" @click="handleLogout"></el-button>
+        </li>
       </ul>
       <ul class="clean-list flex gap-15" v-else>
         <li>
@@ -26,13 +29,30 @@
 </template>
 
 <script>
+import { ElMessage } from 'element-plus'
+import { SwitchButton } from '@element-plus/icons-vue'
+import { authService } from '../services/auth.service'
+
 export default {
+  methods: {
+    handleLogout() {
+      authService.logout()
+        .then(() => {
+          this.$store.commit({ type: 'setUser', user: null })
+          ElMessage.success('Successfully logged out!')
+        })
+        .catch(() => ElMessage.error('Failed to log out!'))
+    }
+  },
   computed: {
     user() {
       return this.$store.getters.user
     },
     profileLink() {
       return '/profile/' + this.user.username
+    },
+    SwitchButton() {
+      return SwitchButton
     }
   }
 }
