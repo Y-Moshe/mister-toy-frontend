8f9a4f9aa14e147b2a2d3e9fd609344a9d5b2839
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-28 20:44:38 +0200

    add socket login onLoadUser

--- src/store/modules/user.store.js
+++ src/store/modules/user.store.js
@@ -1,11 +1,12 @@
-import { authService } from '../../services/auth.service.js'
+import { authService } from '../../services/auth.service'
+import { socketService } from '../../services/socket.service'
 
 export const mutations = {
   setUser: user => ({ type: 'setUser', user }),
 }
 
 export const actions = {
-  setUser: user => ({ type: 'loadUser' }),
+  loadUser: () => ({ type: 'loadUser' }),
 }
 
 export const userModule = {
@@ -17,13 +18,15 @@ export const userModule = {
   mutations: {
     setUser(state, { user }) {
       state.user = user
-    }
+    },
   },
   actions: {
     loadUser({ commit }) {
-      authService.getLoggedinUser()
-        .then(user => commit(mutations.setUser(user)))
-        .catch(() => console.log('[loadUser] Cannot load user'))
+      const user = authService.getLoggedinUser()
+      if (!user) return
+
+      commit(mutations.setUser(user))
+      socketService.login(user._id)
     }
   },
   getters: {
