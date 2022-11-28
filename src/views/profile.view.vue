18295bcf276360ec2b12f41835dd1e4f9961cc98
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-28 20:42:54 +0200

    profile user page (yet to be)

--- src/views/profile.view.vue
+++ src/views/profile.view.vue
@@ -3,5 +3,29 @@
 </template>
 
 <script>
-export default {}
+import { ElMessage } from 'element-plus'
+import { reviewService } from '../services/review.service'
+
+export default {
+  data() {
+    return {
+      reviews: []
+    }
+  },
+  created() {
+    this.loadReviews()
+  },
+  methods: {
+    loadReviews() {
+      reviewService.query({ userId: this.user._id })
+        .then(reviews => this.reviews = reviews)
+        .catch(() => ElMessage.error('Failed to load reviews'))
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.state.getters.user
+    }
+  }
+}
 </script>
